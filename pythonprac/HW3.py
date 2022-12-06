f1b318763e0182876c50bd516ea82edc051351f6
chlrkdgml11 <chlrkdgml11@naver.com>
2022-12-06 19:16:42 +0900

    SpartaCoding_Web - HW3

--- pythonprac/HW3.py
+++ pythonprac/HW3.py
@@ -1,23 +1,10 @@
-# 순위
-# 제목
-# 가수
-
-
 from bs4 import BeautifulSoup
 import requests
-from pymongo import MongoClient
-import certifi
-
-ca = certifi.where()
-client = MongoClient('mongodb+srv://Kanghee:Rkdgml110^@cluster0.e5sej63.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
-db = client.dbsparta
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://www.genie.co.kr/chart/top200?ditc=M&rtm=N&ymd=20210701',headers=headers)
 
-
 soup = BeautifulSoup(data.text, 'html.parser')
-
 songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr')
 
 #body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.number
@@ -29,7 +16,6 @@ songs = soup.select('#body-content > div.newest-list > div > table > tbody > tr'
 #body-content > div.newest-list > div > table > tbody > tr:nth-child(1) > td.info > a.artist.ellipsis
 #body-content > div.newest-list > div > table > tbody > tr:nth-child(2) > td.info > a.artist.ellipsis
 
-
 for song in songs:
     temp_rank = song.select_one('td.number')
     temp_title = song.select_one('td.info > a.title.ellipsis')
@@ -39,6 +25,3 @@ for song in songs:
         title = temp_title.text.strip()
         singer = temp_singer.text
         print(rank, title, singer)
-
-
-

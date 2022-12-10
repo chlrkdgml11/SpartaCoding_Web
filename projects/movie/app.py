21eedb3086a82a936223b2c51b80610747c4eae7
chlrkdgml11 <chlrkdgml11@naver.com>
2022-12-10 23:37:38 +0900

    SpartaCoding_Web - Week 4-13

--- projects/movie/app.py
+++ projects/movie/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 import certifi
+import requests
+from bs4 import BeautifulSoup
+
 
 ca = certifi.where()
-client = MongoClient('mongodb+srv://Kanghee:Rkdgml110^@cluster0.e5sej63.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
+client = MongoClient(
+    'mongodb+srv://Kanghee:Rkdgml110^@cluster0.e5sej63.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=ca)
 db = client.dbsparta
 
 
@@ -15,8 +19,41 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/test', methods=['GET'])
-def test_get():
-    title_receive = request.args.get('title_give')
-    print(title_receive)
-    return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
+@app.route("/movie", methods=["POST"])
+def movie_post():
+    url_receive = request.form['url_give']
+    star_receive = request.form['star_give']
+    comment_receive = request.form['comment_give']
+
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+
+
+    data = requests.get(url_receive, headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    title = soup.select_one('meta[property="og:title"]')['content']
+    image = soup.select_one('meta[property="og:image"]')['content']
+    desc = soup.select_one('meta[property="og:description"]')['content']
+
+    doc = {
+        'title' : title,
+        'image' : image,
+        'desc' : desc,
+        'star' : star_receive,
+        'comment' : comment_receive
+    }
+
+    db.naver_movies.insert_one(doc)
+
+    return jsonify({'msg': '저장 완료!'})
+
+
+@app.route("/movie", methods=["GET"])
+def movie_get():
+    return jsonify({'msg': 'GET 연결 완료!'})
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
